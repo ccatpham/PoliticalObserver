0fbbffca35e31b6ab88155712240e85ef6180a3c
pkhodadoustan <pardis.khodadoustan@student.csulb.edu>
2020-03-01 23:44:40 -0800

    expandable items for Issues

--- src/screens/Issues.js
+++ src/screens/Issues.js
@@ -1,12 +1,139 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  ScrollView,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+//import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
+
+var item_date = 'March, 1, 2020';
+var item_description =
+  'Bacon ipsum dolor amet chuck turducken landjaeger tongue spare ribs. Picanha beef prosciutto meatball turkey shoulder shank salami cupim doner jowl pork belly cow. Chicken shankle rump swine tail frankfurter meatloaf ground round flank ham hock tongue shank andouille boudin brisket. ';
+var item_pro = 'Reasons to vote yes.';
+var item_con = 'Reasons to Vote no.';
+var item_notes = 'Here are some important and controvertial notes to consider.';
+var CONTENT = [
+  {
+    title: 'Issue 1',
+    content: item_description,
+  },
+  {
+    title: 'Issue 2',
+    content: item_description,
+  },
+  {
+    title: 'Issue 3',
+    content: item_description,
+  },
+  {
+    title: 'Issue 4',
+    content: item_description,
+  },
+  {
+    title: 'Issue 5',
+    content: item_description,
+  },
+];
 
 export default class IssuesScreen extends React.Component {
-  render() {
+  state = {
+    activeSections: [],
+  };
+
+  //keeps a list of active (expanded issues)
+  setSections = sections => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  //header of the Issue
+  renderHeader = (section, _, isActive) => {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text>Issues Screen</Text>
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  //content of the issue
+  renderContent(section, _, isActive) {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor">
+        <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  render() {
+    const {activeSections} = this.state;
+
+    return (
+      <View style={styles.container}>
+        <ScrollView contentContainerStyle={{paddingTop: 30}}>
+          <Text style={styles.title}>Vote on Political Issues</Text>
+
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            touchableComponent={TouchableOpacity}
+            expandMultiple={true}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={400}
+            onChange={this.setSections}
+          />
+        </ScrollView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+  },
+});
