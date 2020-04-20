9a486bb453ab39140a9d228c43c426d255712c62
Wesley Swanson <wesleyswanson97@gmail.com>
2020-04-20 01:14:14 -0700

    Installation of victory-native and basic prototype

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
 
 export default class DashboardScreen extends React.Component {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Dashboard Screen</Text>
+        <VictoryPie
+          data={[{x: 'Cats', y: 35}, {x: 'Dogs', y: 40}, {x: 'Birds', y: 55}]}
+        />
       </View>
     );
   }
