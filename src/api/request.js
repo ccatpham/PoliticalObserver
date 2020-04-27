24082372006802d1b6a65be38118ac1367e13b36
Wesley Swanson <wesleyswanson97@gmail.com>
2020-04-27 01:09:47 -0700

    GenderDemographics example

--- src/api/request.js
+++ src/api/request.js
@@ -1,10 +1,10 @@
 const axios = require('axios').default;
 
 // Development Server
-const baseUrl = 'https://political-observer.herokuapp.com/';
+//const baseUrl = 'https://political-observer.herokuapp.com/';
 
 // Local Server
-// const baseUrl = 'http://127.0.0.1:3000/';
+const baseUrl = 'http://127.0.0.1:3000/';
 
 const request = axios.create({baseURL: baseUrl});
 
--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Alert, Text, View} from 'react-native';
+import {VictoryPie} from 'victory-native';
+import pol from '../api/apiConfig';
 
 export default class DashboardScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  componentDidMount() {
+    pol.api
+      .getGenderDemographics()
+      .then(response => {
+        this.setState({data: response});
+      })
+      .catch(error => {
+        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
+          cancelable: false,
+        });
+      });
+  }
+
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
         <Text>Dashboard Screen</Text>
+        <VictoryPie data={this.state.data} />
       </View>
     );
   }
