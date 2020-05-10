9cb03536398ef3af30edf894ecebedcb1a24a362
Catherine Pham <ccatherinepham@gmail.com>
2020-05-10 16:14:00 -0700

    Added dropdown to edit demographics

--- src/screens/Profile/Demographics/EditDemographic.js
+++ src/screens/Profile/Demographics/EditDemographic.js
@@ -10,25 +10,159 @@ import {
 } from 'react-native';
 import pol from '../../../api/apiConfig';
 import {colors} from '../../../styles';
+import {Dropdown} from 'react-native-material-dropdown';
 
 export default class EditDemographic extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      user: {},
       userId: this.props.route.params.user.id,
       demographicId: this.props.route.params.user.demographicId,
       partyAffiliation: '',
-      martialStatus: '',
-      ageRange: '',
+      maritalStatus: '',
+      maritalValue: '',
       education: '',
+      educationValue: '',
       ethnicity: '',
-      incomeLevel: '',
+      ethnicityValue: '',
       occupation: '',
       personalityType: '',
+      personalityTypeValue: '',
       politicalAffiliation: '',
+      partyAffiliationValue: '',
       state: '',
       gender: '',
-      income: '',
+      genderValue: '',
+      partyAffiliationData: [
+        {
+          value: 'Democrat',
+        },
+        {
+          value: 'Republican',
+        },
+        {
+          value: 'Libertarian',
+        },
+        {
+          value: 'Green',
+        },
+        {
+          value: 'Constitution',
+        },
+        {
+          value: 'Unaligned',
+        },
+      ],
+      maritalStatusData: [
+        {
+          value: 'Single',
+        },
+        {
+          value: 'Married',
+        },
+      ],
+      ethnicityData: [
+        {
+          value: 'White',
+        },
+        {
+          value: 'African American',
+        },
+        {
+          value: 'Asian',
+        },
+        {
+          value: 'Native American',
+        },
+        {
+          value: 'Hispanic',
+        },
+        {
+          value: 'Other',
+        },
+      ],
+      educationChoices: [
+        {
+          value: 'None',
+        },
+        {
+          value: 'Diploma',
+        },
+        {
+          value: "Associate's",
+        },
+        {
+          value: "Bachelor's",
+        },
+        {
+          value: "Master's",
+        },
+        {
+          value: 'Doctoral',
+        },
+      ],
+      genderData: [
+        {
+          value: 'Male',
+        },
+        {
+          value: 'Female',
+        },
+        {
+          value: 'Other',
+        },
+      ],
+      personalityTypeData: [
+        {
+          value: 'INTJ',
+        },
+        {
+          value: 'INTP',
+        },
+        {
+          value: 'ENTJ',
+        },
+        {
+          value: 'ENTP',
+        },
+        {
+          value: 'INFJ',
+        },
+        {
+          value: 'INFP',
+        },
+        {
+          value: 'ENFJ',
+        },
+        {
+          value: 'ENFP',
+        },
+        {
+          value: 'ISTJ',
+        },
+        {
+          value: 'ISFJ',
+        },
+        {
+          value: 'ESTJ',
+        },
+        {
+          value: 'ESFJ',
+        },
+        {
+          value: 'ISTP',
+        },
+        {
+          value: 'ISFP',
+        },
+        {
+          value: 'ESTP',
+        },
+        {
+          value: 'ESFP',
+        },
+      ],
     };
   }
 
@@ -36,9 +170,33 @@ export default class EditDemographic extends React.Component {
     this.getDemographic();
   }
 
-  onChangeAge(age) {
+  onChangePartyAffiliation(partyAffiliation) {
     this.setState({
-      age: age,
+      partyAffiliation: partyAffiliation,
+    });
+  }
+
+  onChangeEthnicity(ethnicity) {
+    this.setState({
+      ethnicity: ethnicity,
+    });
+  }
+
+  onChangeEducation(education) {
+    this.setState({
+      education: education,
+    });
+  }
+
+  onChangePersonalityType(personalityType) {
+    this.setState({
+      personalityType: personalityType,
+    });
+  }
+
+  onChangeMaritalStatus(maritalStatus) {
+    this.setState({
+      maritalStatus: maritalStatus,
     });
   }
 
@@ -48,25 +206,52 @@ export default class EditDemographic extends React.Component {
     });
   }
 
-  onChangeIncome(income) {
+  onChangeGender(gender) {
     this.setState({
-      income: income,
+      gender: gender,
     });
   }
+
+  onPressUpdate = () => {
+    let user = this.state.user;
+    user.partyAffiliation = this.state.partyAffiliation;
+    user.maritalStatus = this.state.maritalStatus;
+    user.education = this.state.education;
+    user.ethnicity = this.state.ethnicity;
+    user.occupation = this.state.occupation;
+    user.personalityType = this.state.personalityType;
+    user.politicalAffiliation = this.state.politicalAffiliation;
+    user.state = this.state.state;
+    user.gender = this.state.gender;
+    console.log('demographicId: ' + this.state.demographicId);
+    console.log(this.state.user);
+  };
+
+  onPressSubmit = () => {
+    this.onPressUpdate();
+    pol.api
+      .modifyDemographic(this.state.demographicId, this.state.user)
+      .then(user => {
+        console.log('Demographic Id: ' + this.state.demographicId);
+        console.log('User id: ' + user.id);
+      })
+      .catch(error => {
+        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
+          cancelable: false,
+        });
+      });
+    this.props.navigation.navigate('Profile');
+  };
 
   getDemographic = () => {
     pol.api
       .getDemographicById(this.props.route.params.user.demographicId)
       .then(response => {
-        console.log(response);
-        console.log(response.partyAffiliation);
         this.setState({
           partyAffiliation: response.partyAffiliation,
-          martialStatus: response.maritalStatus,
-          ageRange: response.ageRange,
+          maritalStatus: response.maritalStatus,
           education: response.education,
           ethnicity: response.ethnicity,
-          incomeLevel: response.incomeLevel,
           occupation: response.occupation,
           personalityType: response.personalityType,
           politicalAffiliation: response.politicalAffiliation,
@@ -82,6 +267,13 @@ export default class EditDemographic extends React.Component {
   };
 
   render() {
+    var partyAffiliationChoices = this.state.partyAffiliationData;
+    var ethnicityChoices = this.state.ethnicityData;
+    var maritalChoices = this.state.maritalStatusData;
+    var educationChoices = this.state.educationData;
+    var genderChoices = this.state.genderData;
+    var personalityTypeChoices = this.state.personalityTypeData;
+
     return (
       <View>
         <ScrollView>
@@ -91,72 +283,84 @@ export default class EditDemographic extends React.Component {
               style={styles.textInput}
               placeholder={'Occupation'}
               placeholderTextColor={colors.gray}
+              defaultValue={this.state.occupation}
               onChangeText={occupation => this.onChangeOccupation(occupation)}
-              value={this.state.occupation}
             />
           </View>
-          <TouchableOpacity style={styles.dividers}>
-            <Text style={styles.headingTextStyle}>Political Affiliation</Text>
-            <Text style={styles.fieldTextStyle}>
-              {this.state.politicalAffiliation}
-            </Text>
-          </TouchableOpacity>
-          <Text style={styles.headingTextStyle}>Party Affiliation</Text>
-          <TouchableOpacity>
-            <Text style={styles.fieldTextStyle}>
-              {this.state.partyAffiliation}
-            </Text>
-          </TouchableOpacity>
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Party Affiliation</Text>
+            <Dropdown
+              value={this.state.partyAffiliation}
+              data={partyAffiliationChoices}
+              onChangeText={partyAffiliationValue => {
+                this.onChangePartyAffiliation(partyAffiliationValue);
+                console.log(this.state.partyAffiliation);
+              }}
+            />
+          </View>
           <Text style={styles.headingTextStyle}>State</Text>
           <TouchableOpacity>
             <Text style={styles.fieldTextStyle}>{this.state.state}</Text>
           </TouchableOpacity>
-          <Text style={styles.headingTextStyle}>Ethnicity</Text>
-          <TouchableOpacity>
-            <Text style={styles.fieldTextStyle}>{this.state.ethnicity}</Text>
-          </TouchableOpacity>
-          <Text style={styles.headingTextStyle}>Education</Text>
-          <TouchableOpacity>
-            <Text style={styles.fieldTextStyle}>{this.state.education}</Text>
-          </TouchableOpacity>
-          <Text style={styles.headingTextStyle}>Personality Type</Text>
-          <TouchableOpacity>
-            <Text style={styles.fieldTextStyle}>
-              {this.state.personalityType}
-            </Text>
-          </TouchableOpacity>
-          <Text style={styles.headingTextStyle}>Age</Text>
-          <TextInput
-            style={styles.textInput}
-            placeholder={'Age'}
-            placeholderTextColor={colors.gray}
-            onChangeText={age => this.onChangeAge(age)}
-            keyboardType={'numeric'}
-            value={this.state.age}
-          />
-          <Text style={styles.headingTextStyle}>Gender</Text>
-          <TouchableOpacity>
-            <Text style={styles.fieldTextStyle}>{this.state.gender}</Text>
-          </TouchableOpacity>
-          <View>
-            <Text style={styles.headingTextStyle}>Income Level</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholder={'Income Level'}
-              placeholderTextColor={colors.gray}
-              onChangeText={income => this.onChangeIncome(income)}
-              keyboardType={'numeric'}
-              value={this.state.income}
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Ethnicity</Text>
+            <Dropdown
+              value={this.state.ethnicity}
+              data={ethnicityChoices}
+              onChangeText={ethnicityValue => {
+                this.onChangeEthnicity(ethnicityValue);
+                console.log(this.state.ethnicity);
+              }}
+            />
+          </View>
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Education</Text>
+            <Dropdown
+              value={this.state.education}
+              data={educationChoices}
+              onChangeText={educationValue => {
+                this.onChangeEducation(educationValue);
+                console.log(this.state.education);
+              }}
+            />
+          </View>
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Marital Status</Text>
+            <Dropdown
+              value={this.state.maritalStatus}
+              data={maritalChoices}
+              onChangeText={maritalValue => {
+                this.onChangeMaritalStatus(maritalValue);
+              }}
+            />
+          </View>
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Personality Type</Text>
+            <Dropdown
+              value={this.state.personalityType}
+              data={personalityTypeChoices}
+              onChangeText={personalityTypeValue => {
+                this.onChangePersonalityType(personalityTypeValue);
+                console.log(this.state.personalityType);
+              }}
+            />
+          </View>
+          <View style={styles.dividers}>
+            <Text style={styles.headingTextStyle}>Gender</Text>
+            <Dropdown
+              value={this.state.gender}
+              data={genderChoices}
+              onChangeText={genderValue => {
+                this.onChangeGender(genderValue);
+                console.log(this.state.gender);
+              }}
             />
           </View>
           <View>
-            <TouchableOpacity style={styles.submitButton}>
-              <Text
-                style={styles.submitButtonText}
-                onPress={console.log(this.state.userId)}>
-                {' '}
-                Submit Changes{' '}
-              </Text>
+            <TouchableOpacity
+              style={styles.submitButton}
+              onPress={() => this.onPressSubmit()}>
+              <Text style={styles.submitButtonText}> Submit Changes </Text>
             </TouchableOpacity>
           </View>
         </ScrollView>
@@ -171,6 +375,9 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'black',
     paddingLeft: 5,
+  },
+  dropDownContainerStyle: {
+    marginLeft: 10,
   },
   fieldTextStyle: {
     fontSize: 20,
