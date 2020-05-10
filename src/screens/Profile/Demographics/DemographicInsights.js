a65d37134ac8420d23673003a925e5797bf8a8b4
Wesley Swanson <wesleyswanson97@gmail.com>
2020-05-10 12:07:40 -0700

    Prototype of user insights

--- src/screens/Profile/Demographics/DemographicInsights.js
+++ src/screens/Profile/Demographics/DemographicInsights.js
@@ -6,73 +6,28 @@ import {
   StyleSheet,
   ScrollView,
   Alert,
+  FlatList,
 } from 'react-native';
 import {VictoryAxis, VictoryPie} from 'victory-native';
 import pol from '../../../api/apiConfig';
+import {colors} from '../../../styles';
 export default class DemographicInsights extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userId: this.props.route.params.userId,
       demographicId: this.props.route.params.user.demographicId,
-      genderData: [],
-      educationData: [],
-      maritalData: [],
-      ageData: [],
-      maritalStatus: '',
-      partyData: [],
-      ageRange: '',
-      partyAffiliation: '',
-      education: '',
-      ethnicity: '',
-      incomeLevel: '',
-      occupation: '',
-      personalityType: '',
-      politicalAffiliation: '',
-      state: '',
-      gender: '',
-      maritalStat: 0,
+      data: [],
     };
   }
 
   componentDidMount() {
-    this.getDemographic();
-    this.getGenderStats();
-    this.getPartyStats();
-    this.getEducationStats();
-    this.getMaritalStats();
-    this.getMaritalPercentage();
-  }
-
-  getDemographic = () => {
     pol.api
-      .getDemographicById(this.props.route.params.user.demographicId)
-      .then(response => {
+      .getUserInsights(this.state.userId)
+      .then(data => {
         this.setState({
-          partyAffiliation: response.partyAffiliation,
-          martialStatus: response.maritalStatus,
-          ageRange: response.ageRange,
-          education: response.education,
-          ethnicity: response.ethnicity,
-          incomeLevel: response.incomeLevel,
-          occupation: response.occupation,
-          personalityType: response.personalityType,
-          politicalAffiliation: response.politicalAffiliation,
-          state: response.state,
-          gender: response.gender,
+          data: data,
         });
-      })
-      .catch(error => {
-        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
-          cancelable: false,
-        });
-      });
-  };
-  getGenderStats() {
-    pol.api
-      .getGenderDemographics()
-      .then(response => {
-        this.setState({genderData: response});
       })
       .catch(error => {
         Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
@@ -81,128 +36,46 @@ export default class DemographicInsights extends React.Component {
       });
   }
 
-  getMaritalStats() {
-    pol.api
-      .getMaritalDemographics()
-      .then(response => {
-        this.setState({maritalData: response});
-      })
-      .catch(error => {
-        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
-          cancelable: false,
-        });
-      });
-  }
-
-  getMaritalPercentage() {
-    pol.api
-      .getMaritalDemographicsById(this.state.userId)
-      .then(response => {
-        this.setState({maritalStat: response.maritalStat.toFixed(2)});
-      })
-      .catch(error => {
-        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
-          cancelable: false,
-        });
-      });
-  }
-
-  getPartyStats() {
-    pol.api
-      .getPartyDemographics()
-      .then(response => {
-        this.setState({partyData: response});
-      })
-      .catch(error => {
-        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
-          cancelable: false,
-        });
-      });
-  }
-
-  getEducationStats() {
-    pol.api
-      .getEducationDemographics()
-      .then(response => {
-        console.log(response);
-        this.setState({educationData: response});
-      })
-      .catch(error => {
-        Alert.alert('Error', error.code + ' ' + error.message, [{text: 'OK'}], {
-          cancelable: false,
-        });
-      });
+  renderItem(item) {
+    return (
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemHeaderText}>{item.type}</Text>
+        <View style={styles.itemChartContainer}>
+          <VictoryPie
+            data={item.data}
+            labelRadius={10}
+            width={300}
+            height={300}
+            padding={0}
+            style={{
+              labels: {
+                fontSize: 16,
+                fontWeight: 'bold',
+              },
+            }}
+          />
+        </View>
+        <Text>
+          Index: {item.index}, Percentage: {item.percentage}
+        </Text>
+      </View>
+    );
   }
 
   render() {
     return (
-      <SafeAreaView style={styles.container}>
-        <ScrollView>
-          <View style={styles.shadowContainerColumn}>
-            <Text
-              style={{alignSelf: 'center', fontWeight: 'bold', marginTop: 10}}>
-              Gender
-            </Text>
-            <View style={{marginHorizontal: 15, flexDirection: 'row'}}>
-              <View style={{flex: 1}}>
-                <VictoryPie
-                  width={300}
-                  height={300}
-                  data={this.state.genderData}
-                  colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
-                  labels={({datum}) =>
-                    `${datum.y}` !== '0' ? `${datum.x}` : ''
-                  }
-                />
-                <Text> You are within 30% of users in this gender group.</Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.shadowContainerColumn}>
-            <Text
-              style={{alignSelf: 'center', fontWeight: 'bold', marginTop: 10}}>
-              Party Affiliation
-            </Text>
-            <View style={{marginHorizontal: 15, flexDirection: 'row'}}>
-              <View style={{flex: 1}}>
-                <VictoryPie
-                  width={300}
-                  height={300}
-                  data={this.state.partyData}
-                  labelPosition="centroid"
-                  labelRadius={20}
-                  labels={({datum}) =>
-                    `${datum.y}` !== '0' ? `${datum.x}` : ''
-                  }
-                  colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
-                />
-                <Text> You are within 30% of users in this age group.</Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.shadowContainerColumn}>
-            <Text
-              style={{alignSelf: 'center', fontWeight: 'bold', marginTop: 10}}>
-              Marital
-            </Text>
-            <View style={{marginHorizontal: 15, flexDirection: 'row'}}>
-              <View style={{flex: 1}}>
-                <VictoryPie
-                  width={300}
-                  height={300}
-                  data={this.state.maritalData}
-                  colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
-                  labels={({datum}) =>
-                    `${datum.y}` !== '0' ? `${datum.x}` : ''
-                  }
-                  labelRadius={20}
-                />
-                <Text>You are within {this.state.maritalStat}% of users.</Text>
-              </View>
+      <View style={styles.container}>
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.contentContainer}>
+            <View style={styles.dataContainer}>
+              <FlatList
+                data={this.state.data}
+                renderItem={({item}) => this.renderItem(item)}
+              />
             </View>
           </View>
         </ScrollView>
-      </SafeAreaView>
+      </View>
     );
   }
 }
@@ -210,21 +83,32 @@ export default class DemographicInsights extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    backgroundColor: colors.polWhite,
   },
-  headingTextStyle: {
+  scrollView: {
+    flex: 1,
+    backgroundColor: colors.polWhite,
+  },
+  contentContainer: {
+    flex: 1,
+    margin: 20,
+    backgroundColor: colors.polGray,
+  },
+  comparisonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginBottom: 20,
+    backgroundColor: colors.polGray,
+  },
+  itemContainer: {},
+  itemHeaderText: {
+    textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
-    color: 'black',
-    alignSelf: 'center',
   },
-  shadowContainerColumn: {
-    padding: 10,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignContent: 'center',
-    flex: 3,
-    elevation: 1,
-    position: 'relative',
-    borderBottomWidth: 0,
+  itemChartContainer: {
+    alignItems: 'center',
   },
 });
