8b0c7d13499e1e06fd55fc273d04a530f069f782
Wesley Swanson <wesleyswanson97@gmail.com>
2020-05-12 20:58:07 -0700

    Demographic Insights redone

--- src/screens/Profile/Demographics/DemographicInsights.js
+++ src/screens/Profile/Demographics/DemographicInsights.js
@@ -8,9 +8,9 @@ import {
   Alert,
   FlatList,
 } from 'react-native';
-import {VictoryAxis, VictoryPie} from 'victory-native';
+import {VictoryPie} from 'victory-native';
 import pol from '../../../api/apiConfig';
-import {colors} from '../../../styles';
+import {colors, colorsData} from '../../../styles';
 export default class DemographicInsights extends React.Component {
   constructor(props) {
     super(props);
@@ -37,17 +37,32 @@ export default class DemographicInsights extends React.Component {
   }
 
   renderItem(item) {
+    let key = item.key;
     return (
       <View style={styles.itemContainer}>
         <Text style={styles.itemHeaderText}>{item.type}</Text>
         <View style={styles.itemChartContainer}>
           <VictoryPie
             data={item.data}
-            labelRadius={10}
-            width={300}
-            height={300}
+            colorScale={colorsData[item.key]}
+            width={150}
+            height={150}
+            innerRadius={50}
+            padAngle={2}
+            labelRadius={20}
+            labels={() => ''}
             padding={0}
             style={{
+              parent: {
+                shadowColor: colors.black,
+                shadowOffset: {
+                  width: 0,
+                  height: 2,
+                },
+                shadowOpacity: 0.25,
+                shadowRadius: 3.84,
+                elevation: 5,
+              },
               labels: {
                 fontSize: 16,
                 fontWeight: 'bold',
@@ -55,27 +70,45 @@ export default class DemographicInsights extends React.Component {
             }}
           />
         </View>
-        <Text>
-          Index: {item.index}, Percentage: {item.percentage}
-        </Text>
+        <View style={styles.itemKeyContainer}>
+          <FlatList
+            data={item.data}
+            renderItem={({item, index}) => {
+              return (
+                <View style={styles.keyContainer}>
+                  <View
+                    style={[
+                      styles.keyColor,
+                      {backgroundColor: colorsData[key][index]},
+                    ]}
+                  />
+                  <Text style={styles.keyText}>{item.x}</Text>
+                </View>
+              );
+            }}
+            numColumns={4}
+            columnWrapperStyle={{
+              justifyContent: 'space-between',
+              alignItems: 'flex-start',
+            }}
+          />
+        </View>
       </View>
     );
   }
 
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           <View style={styles.contentContainer}>
-            <View style={styles.dataContainer}>
-              <FlatList
-                data={this.state.data}
-                renderItem={({item}) => this.renderItem(item)}
-              />
-            </View>
+            <FlatList
+              data={this.state.data}
+              renderItem={({item}) => this.renderItem(item)}
+            />
           </View>
         </ScrollView>
-      </View>
+      </SafeAreaView>
     );
   }
 }
@@ -92,8 +125,8 @@ const styles = StyleSheet.create({
   },
   contentContainer: {
     flex: 1,
-    margin: 20,
-    backgroundColor: colors.polGray,
+    marginVertical: 10,
+    backgroundColor: colors.polWhite,
   },
   comparisonContainer: {
     flex: 1,
@@ -102,13 +135,56 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     backgroundColor: colors.polGray,
   },
-  itemContainer: {},
+  itemContainer: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    padding: 10,
+    backgroundColor: colors.polWhite,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
   itemHeaderText: {
     textAlign: 'center',
     fontSize: 20,
     fontWeight: 'bold',
   },
   itemChartContainer: {
+    flex: 1,
+    paddingVertical: 10,
+    justifyContent: 'center',
     alignItems: 'center',
+  },
+  itemKeyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  keyContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 4,
+  },
+  keyColor: {
+    marginRight: 4,
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    shadowColor: colors.black,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  keyText: {
+    textAlign: 'center',
   },
 });
